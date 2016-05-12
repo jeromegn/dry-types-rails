0779c0b914bd6ab815c7d7ab707659a5b319e657
Phil <pnomolos@gmail.com>
2016-05-12 14:27:41 -0700

    Left old registry code in - no longer needed :)

--- lib/dry/types/rails.rb
+++ lib/dry/types/rails.rb
@@ -1,11 +1,9 @@
 require "dry/types/rails/version"
 
 require 'dry/types'
-require 'dry/types/rails/registry'
 require 'dry/types/rails/railtie'
 
 module Dry
   module Types
-    container.config.registry = ::Dry::Types::Rails::Registry.new
   end
 end
